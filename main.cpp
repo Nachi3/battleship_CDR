a4fa3ab26417982370c06f2893459d685a4b5579
Ignacio Pardo <ignacio.pardo1901@alumnos.ubiobio.cl>
2023-05-21 02:40:52 -0400

    Creacion del tablero con rellenado aleatorio e impresion en consola

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 // first commit
 #include <iostream>
-/*
+#include <windows.h>
+#include "tablero.cpp"
+
 using namespace std;
 
 int main(){
     cout << "Hola mundo";
+    int option = 2;
+    Battleship mybattleship;
+
+    cout << "\n¿Como desea generar su tablero?:";
+    cout << "\n(1) Aleatoriamente\n(2) Manualmente\nEliga una opcion: ";
+
+    while(option != 1) {
+        cin >> option;
+        if (option == 1){
+            mybattleship.GenerarTableroAleatorio();
+        } else {
+            cout << "\nIngrese una opcion valida: ";
+        }
+    }
+
     return 0;
 }
-*/
+
